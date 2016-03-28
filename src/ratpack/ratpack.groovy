157d55d3b755e6e35fb12e0b7df560ef03cd9dac
Ievgenii Shepeliuk <ishepeliuk@ITs-MacBook-Pro.local>
2016-03-28 10:50:45 +0300

    heroku

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -4,9 +4,9 @@ import ratpack.ssl.SSLContexts
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-    serverConfig {
-        ssl SSLContexts.sslContext(this.class.getResource("/maapbot.jks"), "changeit")
-    }
+//    serverConfig {
+//        ssl SSLContexts.sslContext(this.class.getResource("/maapbot.jks"), "changeit")
+//    }
     handlers {
         all RequestLogger.ncsa()
         get(":name") {
