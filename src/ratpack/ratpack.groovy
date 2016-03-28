dc46e597d079cb09ccadd2ba3d98052c0d8d8f70
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-03-28 21:49:27 +0300

    Botan.IO

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,5 +1,7 @@
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.TelegramBotAdapter
+import com.pengrad.telegrambot.model.request.ParseMode
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup
 import me.shib.java.lib.botan.Botan
 import org.slf4j.LoggerFactory
 import ratpack.handling.RequestLogger
@@ -30,15 +32,18 @@ ratpack {
                 logger.info("update=$it")
                 def message = it['message']
 
-                telegramBot.sendMessage(message['chat']['id'] as Long, "You've just said ${message['text']}")
+                telegramBot.sendMessage(
+                    message['chat']['id'] as Long, "You've just said *${message['text']}*",
+                    ParseMode.Markdown, false, null,
+                    new ReplyKeyboardMarkup([message['text'], "Cool", "It sucks"] as String[])
+                )
 
-//                try {
-//                    def botanResponse = botan.track(message.get('from').asLong(), "message", message)
-//                    logger.info("botan response isAccepted=${botanResponse.accepted}, info=${botanResponse.info}, status=${botanResponse.status}")
-//                    botanResponse.toString()
-//                } catch (Exception e) {
-//                    logger.error("Cannot track message to Botan", e)
-//                }
+                try {
+                    def botanResp = botan.track(message['from']['id'] as Long, "message", it)
+                    logger.info("botan response isAccepted=${botanResp.accepted}, info=${botanResp.info}, status=${botanResp.status}")
+                } catch (Exception e) {
+                    logger.error("Cannot track message to Botan", e)
+                }
 
                 response.send("OK")
             }
