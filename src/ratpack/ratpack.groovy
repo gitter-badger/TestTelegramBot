03f9e81f144cf1228878fc3f26b9d12029f4c0a2
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-03-28 21:25:10 +0300

    Botan.IO

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,42 +1,41 @@
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.TelegramBotAdapter
-import com.pengrad.telegrambot.model.Message
+import me.shib.java.lib.botan.Botan
 import org.slf4j.LoggerFactory
 import ratpack.handling.RequestLogger
 
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.jackson.Jackson.jsonNode
 
-import me.shib.java.lib.botan.Botan
-
 def logger = LoggerFactory.getLogger("ua.eshepelyuk")
-def botanToken = "eLCwUk8FyyShxL88OJ2RS-Q1YXgD_eHQ";
-def Botan botan = new Botan(botanToken);
 
 ratpack {
 
     bindings {
         TelegramBot bot = TelegramBotAdapter.build("205365091:AAHhR6iyhWwK9pdv0FEvrKiyng0yHeI4avc")
         bindInstance TelegramBot, bot
+
+        Botan botan = new Botan("AvVoNrWQ5PQgC-FxDCeoXf_ytf7m3_mJ")
+        bindInstance(botan)
     }
 
     handlers {
         all RequestLogger.ncsa()
 
-        post("webhook") { TelegramBot telegramBot ->
+        post("webhook") { TelegramBot telegramBot, Botan botan ->
             logger.info("webhook called length=${request.contentLength}, type=${request.contentType}")
 
-            context.parse(jsonNode())
-                    .onError {
+            context.parse(jsonNode()) onError {
                 logger.error("webhook exception", it)
                 response.status(500).send(it.message)
             } then {
                 logger.info("webhook update=${it}")
-                telegramBot.sendMessage(it.get('message').get('chat').get('id').asLong(), "You've just said ${it.get('message').get('text').asText()}"
-                )
+                def message = it.get("message")
+
+                telegramBot.sendMessage(message.get('chat').get('id').asLong(), "You've just said ${message.get('text').asText()}")
 
                 try {
-                    botan.track(it.get('message').get('from').asLong(), "message", it.get('message'));
+                    botan.track(message.get('from').asLong(), "message", message)
                 } catch (Exception e) {
                     logger.error("Cannot track message to Botan", e)
                 }
