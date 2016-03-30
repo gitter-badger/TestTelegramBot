c4089aa069ec5436b2a51cf1398fd2fd9012a7fb
solntsev <aleksey.solntsev@gmail.com>
2016-03-30 23:47:15 +0300

    Added auth support and venuesSeatch request. Be aware incorect parsing of response by foursquare-api-java library

--- src/test/groovy/FousquareTest.groovy
+++ src/test/groovy/FousquareTest.groovy
@@ -1,20 +1,35 @@
+import com.likethecolor.alchemy.api.Client
 import fi.foyt.foursquare.api.FoursquareApi
 import fi.foyt.foursquare.api.Result
 import fi.foyt.foursquare.api.entities.CompactVenue
 import fi.foyt.foursquare.api.entities.VenuesSearchResult
+import org.yaml.snakeyaml.Yaml
 
-FoursquareApi foursquareApi = new FoursquareApi("Client ID", "Client Secret", "Callback URL");
+def yaml = new Yaml().load(MaapBotConfig.getResourceAsStream("/maapbot.yaml"))
 
-Result<VenuesSearchResult> result = foursquareApi.venuesSearch("Obolon", null, null, null, null, null, null, null, null, null, null, null, null);
+FoursquareApi foursquareApi = new FoursquareApi(
+        yaml.maapbot.foursquareClientId,
+        yaml.maapbot.foursquareClientSecret,
+        "Callback URL");
+
+def request = ['ll':'50.50,30.50','near':'Оболонь','section':'food','query':'мясо']
+
+//One of food, drinks, coffee, shops, arts, outdoors, sights, trending or specials,
+//nextVenues (venues frequently visited after a given venue),
+//or topPicks (a mix of recommendations generated without a query from the user). Choosing one
+//of these limits results to venues with the specified category or property.
+
+Result<VenuesSearchResult> result = foursquareApi.venuesExplore(
+        '50.50,30.50', null, null, null, 1000, 'food', ' пицца',10, null) as Result<VenuesSearchResult>;
 
 if (result.getMeta().getCode() == 200) {
     for (CompactVenue venue : result.getResult().getVenues()) {
-        System.out.println(venue.getName());
+        println venue.getName()
     }
 } else {
-    System.out.println("Error occured: ");
-    System.out.println("  code: " + result.getMeta().getCode());
-    System.out.println("  type: " + result.getMeta().getErrorType());
-    System.out.println("  detail: " + result.getMeta().getErrorDetail());
+    println "Error occured: "
+    println "  code: " + result.getMeta().getCode()
+    println "  type: " + result.getMeta().getErrorType()
+    println "  detail: " + result.getMeta().getErrorDetail()
 }
 
